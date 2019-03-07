86adcf212ce3c27259ac532f58c1014f7c41b05b
alinastarkov <hayleeluu@gmail.com>
2019-03-06 19:01:46 -0500

    Added graph and drop down selection

--- app.py
+++ app.py
@@ -14,5 +14,49 @@ print(df)
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+app.layout = html.Div([
+    html.Div([
+            dcc.Dropdown(
+                id='xaxis',
+                options=[{'label': i, 'value': i} for i in df.columns.values],
+                value='aaa'
+            ),
+             dcc.Dropdown(
+                id='yaxis',
+                options=[{'label': i, 'value': i} for i in df.columns.values],
+                value='aaa'
+            )
+        ],
+        style={'width': '50%', 'display': 'inline-block'}),
+    dcc.Graph(id='mgp-w')
+])
+
+@app.callback(
+    dash.dependencies.Output('mgp-w', 'figure'),
+    [dash.dependencies.Input('xaxis', 'value'),
+     dash.dependencies.Input('yaxis', 'value')])
+def update_graph(xaxis, yaxis):
+
+    return {
+        'data': [go.Scatter(
+            x=df[xaxis],
+            y=df[yaxis],
+            text=df['model'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': xaxis},
+                yaxis={'title': yaxis},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+        )
+    }
+
 if __name__ == '__main__':
     app.run_server(debug=True)
