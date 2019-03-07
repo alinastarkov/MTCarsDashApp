ace3db3391f7aaea38ac178b22fbd1f785d581c0
alinastarkov <hayleeluu@gmail.com>
2019-03-06 20:47:05 -0500

    Fix KeyError by adding initial values for the graph

--- app.py
+++ app.py
@@ -6,28 +6,36 @@ import plotly.graph_objs as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv('mtcars.csv')
 
-print(df)
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
+app.layout = html.Div(style={'backgroundColor': 'white'}, children=[
+    html.H1(children="All you need to know about Motor Trend Cars", style={
+            'font-weight': 'bold',
+            'font-family': 'helvetica',
+            'textAlign': 'center',
+            'color': '#1B1B1C'
+        }),
     html.Div([
             dcc.Dropdown(
                 id='xaxis',
                 options=[{'label': i, 'value': i} for i in df.columns.values],
-                value='aaa'
-            ),
-             dcc.Dropdown(
-                id='yaxis',
-                options=[{'label': i, 'value': i} for i in df.columns.values],
-                value='aaa'
+                value='mpg'
             )
         ],
-        style={'width': '50%', 'display': 'inline-block'}),
+        style={'width': '30%', 'display': 'inline-block'}),
+    html.Div([
+            dcc.Dropdown(
+                id='yaxis',
+                options=[{'label': i, 'value': i} for i in df.columns.values],
+                value='wt'
+            )
+        ],
+        style={'width': '30%', 'display': 'inline-block',  'float': 'left'}),
     dcc.Graph(id='mgp-w')
 ])
 
@@ -54,8 +62,7 @@ def update_graph(xaxis, yaxis):
                 yaxis={'title': yaxis},
                 margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
                 legend={'x': 0, 'y': 1},
-                hovermode='closest'
-        )
+                hovermode='closest'        )
     }
 
 if __name__ == '__main__':
